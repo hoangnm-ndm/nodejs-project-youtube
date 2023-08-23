cebb9e5c7eb0ebe9db6dfa1ec05e247cd49b814e
hoangm.ndm <hoangm.ndm@gmail.com>
2023-08-23 15:38:14 +0700

    update cloudinary

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,9 +2,11 @@ import express from "express";
 import routerProduct from "./product.js";
 import routerAuth from "./auth.js";
 import routerCategory from "./category.js";
+import routerImages from "./upload.js";
 const router = express.Router();
 
-router.use("/product", routerProduct);
-router.use("/category", routerCategory);
+router.use("/products", routerProduct);
+router.use("/categories", routerCategory);
 router.use("/auth", routerAuth);
+router.use("/images", routerImages);
 export default router;
